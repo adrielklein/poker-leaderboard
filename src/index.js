6f57814c0bf6f2db78ec9b0fe713df46d111885a
Adriel Klein <adrielmklein@gmail.com>
2019-02-21 20:27:38 -0800

    Revert "add linter"
    This reverts commit 413a360ef0d7c950a775d6692ca4b14762dd4159.

--- src/index.js
+++ src/index.js
@@ -4,21 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const rootEl = document.getElementById('root');
-
-ReactDOM.render(<App />, rootEl);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    const NextApp = require('./App').default;
-    ReactDOM.render(
-      <NextApp />,
-      rootEl,
-    );
-  });
-}
