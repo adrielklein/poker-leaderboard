199da5cc54d6ef411eba595ff20fbd102631bb42
Adriel Klein <aklein@marketo.com>
2019-02-24 01:06:53 -0800

    add proxy back in to see what is breaks

--- src/data.js
+++ src/data.js
@@ -1,23 +1,3 @@
-export const rows = [
-    {
-        "id": 0,
-        "country": "USA",
-        "playerName": "playerA",
-        "amount": 9882,
-    },
-    {
-        "id": 1,
-        "country": "Canada",
-        "playerName": "playerB",
-        "amount": 6402,
-    },
-    {
-        "id": 2,
-        "country": "Mexico",
-        "playerName": "playerC",
-        "amount": 5530,
-    }];
-
 export const columns = [
     { name: 'playerName', title: 'Player' },
     { name: 'country', title: 'Country' },
